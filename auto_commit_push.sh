29eef8ba68171405fc03ee059e87a86508391559
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-02-10 19:58:01 +0100

    some commit message

--- auto_commit_push.sh
+++ auto_commit_push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#echo Enter branch name
-#read branch
+echo Enter branch name
+read branch
 echo 'Enter commit message'
 read commit_message
 
 git add .
 git commit -m"$commit_message"
-git push
+git push origin $branch_name
 
 $SHELL
